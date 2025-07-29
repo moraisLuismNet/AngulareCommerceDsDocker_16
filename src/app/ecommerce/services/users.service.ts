4f225e6dd3dbe911bf9a22be4d5feb5562bdff01
moraisLuismNet <morais.luism.net@gmail.com>
2025-07-29 16:08:45 +0200

    29.07.2025_Init+README

---
+++ src/app/ecommerce/services/users.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, Observable, of, tap } from 'rxjs';
+import { IUser } from '../ecommerce.interface';
+import { environment } from 'src/environments/environment';
+import { AuthGuard } from '../../guards/auth-guard.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  private readonly urlAPI = environment.urlAPI;
+
+  constructor(private http: HttpClient, private authGuard: AuthGuard) {}
+
+  private getHeaders(): HttpHeaders {
+    const token = this.authGuard.getToken();
+    return new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+  }
+
+  getUsers(): Observable<IUser[]> {
+    const headers = this.getHeaders();
+    return this.http.get<IUser[]>(`${this.urlAPI}Users`, { headers }).pipe(
+      map((response) => {
+        // The API returns the array directly, no need to access .$values
+        return Array.isArray(response) ? response : [];
+      }),
+      catchError((error) => {
+        console.error('Error in the request:', error);
+        return of([]);
+      })
+    );
+  }
+
+  deleteUser(email: string): Observable<any> {
+    const headers = this.getHeaders();
+    return this.http.delete(
+      `${this.urlAPI}Users/${encodeURIComponent(email)}`,
+      { headers }
+    );
+  }
+}
